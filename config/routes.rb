3dcd2d33087178b537415ca57e13c347f3453fbb
Tony Moey <moeyster@gmail.com>
2017-11-17 10:35:18 +0800

    Added a home page with sticky nav, banner image, 3 steps and call to action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dashboards
 
   root 'static_pages#index'
 end
